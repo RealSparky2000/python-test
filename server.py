9c3c620df23bc87f5d3412248e0f049b362bc468
Glitch (python-te) <none>
2018-12-12 20:28:59 +0000

    💷🌞 Updated with Glitch

--- server.py
+++ server.py
@@ -1,12 +1,21 @@
 import discord
 from discord.ext import commands
+from discord.ext.commands import Bot
+import asyncio
+import os 
 
-TOKEN = process.env.SECRET
-
-client = commands.Bot(command_prefix = 'm.')
-
+Client = discord.Client ()
+client = commands.bot(command_prefix='m.')
 @client.event
-async def on_ready():
-    print ("Bot Is Ready!")
-
-client.run(TOKEN)
+async def on_ready();
+     print("Bot has been launched!")
+     await client.change_presence(game=discord.Game(name="New me on Python"))
+    
+@client.event
+async def on_message(message) :
+  if message.content.startsWith('m.python') :
+  msg = "Yeah, i have been launched on Python 3.6.7 (discord.py)"
+  await client.send_message(message.channel, msg)
+  
+  client.run(os.getenv(SECRET))
+  
